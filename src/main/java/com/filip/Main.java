3715aaecb499a66a0ccfa7f8546a2ff8dd63815d
pawelgrzeda <60094765+pawelgrzeda@users.noreply.github.com>
2020-06-25 14:48:08 +0200

    Add files via upload

---
+++ src/main/java/com/filip/Main.java
@@ -0,0 +1,10 @@
+package com.filip;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        System.out.println("Hello World in version "+version);
+    }
+
+}
